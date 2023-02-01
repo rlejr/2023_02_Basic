b9dc5c12d40981906b7db34dccc26919244f8c05
rlejr <rlejr159753@gmail.com>
2023-02-01 15:53:08 +0900

    게시판 명령어 생성

--- src/com/KoreaIT/java/Main.java
+++ src/com/KoreaIT/java/Main.java
@@ -1,6 +1,9 @@
 package com.KoreaIT.java;
 
+
 import java.util.Scanner;
+
+
 
 public class Main {
 
@@ -13,8 +16,26 @@ public class Main {
 			System.out.printf("명령어를 입력해주세요 : ");
 			String comand = sc.nextLine();
 
+			if (comand.equals("article list")) {
+				System.out.println("게시글이 없습니다.");
+			}
+			if (comand.equals("article write")) {
+				System.out.printf("제목 : ");
+
+				String 제목 = sc.nextLine();
+
+				System.out.printf("내용 : ");
+
+				String 내용 = sc.nextLine();
+
+				System.out.println();
+			}
+
 			if (comand.equals("system exit")) {
 				break;
+
+			} else {
+				System.out.println("\n존재하지 않는 명령어 입니다\n");
 			}
 
 		}
